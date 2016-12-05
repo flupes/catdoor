1a88293ad35ccbeb64e6e42a6842774ebc413beb
Demo User <debian@dog4cat.localdomain>
2016-12-04 21:40:33 -0800

    added script to start the program in a tmux session

---
+++ scripts/start-catdoor.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+session_name=catdoor
+
+catdir=/home/debian/catdoor
+today=`date +%Y-%m-%d_%H%M%S`
+catlog=${catdir}/logs/${today}.log
+
+byobu new-session -d -s ${session_name} -c ${catdir}
+
+byobu new-window -t ${session_name}:1 -n pusher "/usr/bin/python -u ${catdir}/python/catdoor_pushbullet.py | tee ${catlog}"
+#tmux new-window -t ${session_name}:1 -n pusher -c ${catdir}
+#tmux send-keys -t ${session_name}:1 "/usr/bin/python ${catdir}/python/catdoor_pushbullet.py" enter
+
+byobu new-window -d -t ${session_name}:2 -n top htop
+
+
