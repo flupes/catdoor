ca4ca267bf6971f3c0f3679aef6b349274a2253f
Lorenzo Flueckiger <lorenzo.flueckiger@gmail.com>
2017-11-05 18:49:36 -0800

    added RTC, LED status and daylight control

--- catdoor_v2/app/src/catdoor.h
+++ catdoor_v2/app/src/catdoor.h
@@ -171,8 +171,8 @@ class Solenoids {
   }
 
   void open(bool boost = false) {
-    PRINT("Solenoids state = ");
-    PRINTLN(state);
+    // PRINT("Solenoids state = ");
+    // PRINTLN(state);
     if (boost) {
       factor = 4;
     } else {
@@ -224,6 +224,66 @@ class Solenoids {
       if ((now - stop_time) > COOLDOWN_DURATION) {
         state = OFF;
       }
+    }
+  }
+};
+
+class LedCtrl {
+  static const uint8_t PIN_L = 11;
+
+ public:
+  typedef enum { MANUAL, PULSATING, FLASHING } mode_t;
+  mode_t mode;
+  uint16_t period;
+  uint16_t ontime;
+  uint16_t offtime;
+  unsigned long last;
+
+  LedCtrl() : mode(MANUAL) {}
+
+  void on() {
+    mode = MANUAL;
+    pwm_set(PIN_L, 500);
+  }
+  void off() {
+    mode = MANUAL;
+    pwm_set(PIN_L, 0);
+  }
+  void pulse(uint16_t period_ms) {
+    period = period_ms;
+    mode = PULSATING;
+    last = millis();
+  }
+
+  void flash(uint16_t on_time_ms, uint16_t off_time_ms) {
+    ontime = on_time_ms;
+    offtime = off_time_ms;
+    mode = FLASHING;
+    last = millis();
+  }
+
+  void update() {
+    static bool on = false;
+    if (mode == MANUAL) return;
+    unsigned long now = millis();
+    if (mode == FLASHING) {
+      if (on && ((now - last) > ontime)) {
+        pwm_set(PIN_L, 0);
+        last = now;
+        on = false;
+      }
+      if (!on && ((now - last) > offtime)) {
+        pwm_set(PIN_L, 500);
+        last = now;
+        on = true;
+      }
+      return;
+    }
+    if (mode == PULSATING) {
+      unsigned long elapsed = now - last;
+      uint16_t v = (uint16_t)(
+          256 * (1.0 + sin(2.0 * 3.141592 * (float)elapsed / period)));
+      pwm_set(PIN_L, v);
     }
   }
 };
--- catdoor_v2/app/src/main.cpp
+++ catdoor_v2/app/src/main.cpp
@@ -1,11 +1,17 @@
-#define USE_SERIAL 1
+// #define USE_SERIAL 1
 #include "catdoor.h"
+#include "rtc.h"
 
 #define USE_ACCEL 1
 #define USE_PROXIM 1
 
+static const HourMinute morning(8, 30);
+static const HourMinute evening(19, 0);
+
 Solenoids *Solenoids::instance = 0;
 // global objects...
+CatTime rtc;
+LedCtrl status_led;
 Accel accel_sensor = Accel();
 Proxim proxim_sensor = Proxim();
 Solenoids sol_actuators = Solenoids();
@@ -25,6 +31,12 @@ void setup() {
     ;  // wait for serial port to connect. Needed for native USB port only
   }
 #endif
+
+  if (!rtc.begin()) {
+    Serial.println("Couldn't find RTC!");
+    while (1)
+      ;
+  }
 
 #ifdef USE_ACCEL
   // Accelerometer setup
@@ -68,19 +80,42 @@ void setup() {
   proxim_sensor.setLowThreshold(0);
   proxim_sensor.setHighThreshold(Proxim::THRESHOLD);
   proxim_sensor.activateProximityThresholdInterrupt();
+
+  // start considering we are in the dark
+  status_led.flash(80, 2900);
 #endif
 }
 
 void loop() {
   static bool cat_exiting = false;
+  static bool daylight = false;
+  static unsigned long last_time = millis();
   static unsigned long action_time = millis();
+
+  unsigned long now = millis();
+  if ((now - last_time) > 10 * 1000) {
+    // we are up to the minute
+    HourMinute lt = rtc.localTime();
+    if (lt > morning && lt < evening) {
+      if (!daylight) {
+        // PRINTLN("PULSE");
+        daylight = true;
+        status_led.pulse(5000);
+      }
+    } else {
+      // PRINTLN("FLASH");
+      daylight = false;
+      status_led.flash(80, 2900);
+    }
+    // PRINTLN(daylight ? "DAY" : "DARK");
+    last_time = now;
+  }
 
   if (proxim_sensor.new_state) {
     proxim_sensor.process();
-    if (proxim_sensor.state == Proxim::CAT) {
-      if (accel_sensor.state == Accel::CLOSED) {
-        sol_actuators.open();
-      }
+    if (daylight && proxim_sensor.state == Proxim::CAT &&
+        accel_sensor.state == Accel::CLOSED) {
+      sol_actuators.open();
     }
   }
 
@@ -111,6 +146,8 @@ void loop() {
   }
 
   sol_actuators.process();
+
+  status_led.update();
 
   delay(20);
 }
