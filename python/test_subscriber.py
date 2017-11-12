ac29562ece9ec240587f2e0587197c3941935dd9
Demo User <debian@dog4cat.localdomain>
2017-11-11 17:40:00 -0800

    added subscribers for catdoor v2 message

--- python/test_subscriber.py
+++ python/test_subscriber.py
@@ -6,17 +6,36 @@ def catdoor_avgproxim(msg):
 def catdoor_state(msg):
     print ("Got new state : "+msg.payload)
 
+def catdoor_heartbeat(msg):
+    print ("Got new heartbeat : "+msg.payload)
+
+def catdoor_message(msg):
+    print ("Got new message : "+msg.payload)
+
+def catdoor_solenoids(msg):
+    print ("Got new solenoids : "+msg.payload)
+
+def catdoor_proximity(msg):
+    print ("Got new proximity : "+msg.payload)
+
+def catdoor_doorstate(msg):
+    print ("Got new doorstate : "+msg.payload)
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with results code "+str(rc))
-    client.subscribe("/catdoor/#")
+    client.subscribe("/catdoor2/#")
     
 def on_message(client, userdata, msg):
     userdata[msg.topic](msg)
     
 topiclist = { "/catdoor/avgproxim" : catdoor_avgproxim,
-              "/catdoor/state" : catdoor_state
-              }
+              "/catdoor/state" : catdoor_state,
+              "/catdoor2/heartbeat" : catdoor_heartbeat,
+              "/catdoor2/message" : catdoor_message,
+              "/catdoor2/solenoids" : catdoor_solenoids,
+              "/catdoor2/doorstate" : catdoor_doorstate,
+              "/catdoor2/proximity" : catdoor_proximity
+}
     
 client = mqtt.Client("CatdoorListener", True, topiclist)
 client.on_connect = on_connect
