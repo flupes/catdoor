8a249f39cc200702548637303b8bb5d14830f26e
Lorenzo Flueckiger <lorenzo.flueckiger@gmail.com>
2017-11-18 16:08:18 -0800

    adapted program to new catdoor v2 states (this version will not work with the old catdoor)

--- python/catdoor_pushbullet.py
+++ python/catdoor_pushbullet.py
@@ -36,7 +36,7 @@ def pb_publish(title, msg):
             print "Unable to send notification: %s" % ex
             print "Will try again in 2 minutes"
             sleep(120)
-        
+
 def publish(title, msg):
     thread = threading.Thread(target=pb_publish, args=[title, msg])
     thread.start()
@@ -54,14 +54,14 @@ class Heartbeat(object):
         msg = "Catdoor is alive (first heartbeat @ " + time2str(self.last_beat) + ")"
         print(time2str(localtime()) + " | " + msg)
         publish(title, msg)
-    
+
     def missing_heartbeat(self):
         self.alive = False
         title = "Catdoor Error"
         msg = "Catdoor considered dead (last published heartbeat @ "+time2str(self.last_beat)+")"
         print(time2str(localtime())+" | " +msg)
         publish(title, msg)
-              
+
     def heartbeat(self, msg):
         self.last_beat = localtime()
         if not self.alive:
@@ -70,63 +70,101 @@ class Heartbeat(object):
             self.timer.cancel()
         self.timer = threading.Timer(self.duration, self.missing_heartbeat)
         self.timer.start()
-        print(time2str(self.last_beat)+" | got heartbeat"+
-              " | average proximity = "+msg.payload)
+        print(time2str(self.last_beat)+" | got heartbeat msg : "+msg.payload)
 
 class DoorState(object):
     def __init__(self, max_opened_state_sec):
         self.state = None
-        self.up = None
+        self.open_out = None
+        self.open_in = None
+        self.jammed = None
         self.timer = None
         self.duration = max_opened_state_sec
 
-    def door_stuck(self):
-        self.up = False
+    def door_jammed(self):
+        self.jammed = True
         title = "Catdoor Warning"
-        msg = "door may be stuck in opened position!"
+        msg = "Door did not close correctly! @ "+time2str(localtime())
+        print(time2str(localtime())+" | " + msg)
+        publish(title, msg)
+
+    def door_unstuck(self):
+        self.jammed = False
+        title = "Catdoor Notification"
+        msg = "Door closed normally after being stuck @ "+time2str(localtime())
         print(time2str(localtime())+" | " + msg)
         publish(title, msg)
 
     def door_cycle(self):
-        global twilioClient
-        self.up = False
-        print(time2str(localtime())+" | door cycle complete.")
-        title = "Catdoor Cycled"
-        msg = "Misty just got in @ "+time2str(localtime())
+        if self.open_out:
+            self.open_out = False
+            direction = "OUT"
+            msg = "Misty went out for a walk @ "+time2str(localtime())
+        if self.open_in:
+            self.open_in = False
+            direction = "IN"
+            msg = "Misty came in for confort @ "+time2str(localtime())
+        print(time2str(localtime())+" | door cycle " + direction + " complete.")
+        title = "Catdoor Cycled " + direction
         publish(title, msg)
-        
+
     def new_state(self, msg):
-        print (time2str(localtime())+" | new state = "+msg.payload)
-        self.state = msg.payload
-        if self.state == "open":
-            self.up = True
-            self.timer = threading.Timer(self.duration, self.door_stuck)
-            self.timer.start()
-        if self.state == "closed":
-            if self.up == True:
+        print (time2str(localtime())+" | got new doorstate : "+msg.payload)
+        self.state = msg.payload.split()[2]
+        if self.state == "OPEN_OUT":
+            self.open_out = True
+        if self.state == "OPEN_IN":
+            self.open_in = True
+        if self.state == "OPEN_OUT" or self.state == "AJAR_OUT":
+            if self.timer != None:
+                self.timer = threading.Timer(self.duration, self.door_jammed)
+                self.timer.start()
+        if self.state == "CLOSED":
+            print(time2str(localtime())+" | door back to closed position.")
+            if self.timer != None:
                 self.timer.cancel()
+                self.timer = None
+            if self.jammed == True:
+                self.door_unstuck()
+            if self.open_in == True or self.open_out == True:
                 self.door_cycle()
-            else:
-                print(time2str(localtime())+" | door back to closed position...")
+
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     client.subscribe("/catdoor/#")
-    
+
 def on_message(client, userdata, msg):
     userdata[msg.topic](msg)
 
-beatcheck = Heartbeat(60)
-doorstate = DoorState(6)
-               
-topiclist = { "/catdoor/avgproxim" : beatcheck.heartbeat,
-              "/catdoor/state" : doorstate.new_state
-              }
-    
+def catdoor_battery_v(msg):
+    print (time2str(localtime())+" | got new battery_v : "+msg.payload)
+
+def catdoor_message(msg):
+    print (time2str(localtime())+" | got new message : "+msg.payload)
+
+def catdoor_solenoids(msg):
+    print (time2str(localtime())+" | got new solenoids : "+msg.payload)
+
+def catdoor_proximity(msg):
+    print (time2str(localtime())+" | got new proximity : "+msg.payload)
+
+beatcheck = Heartbeat(90)
+doorstate = DoorState(60)
+
+topiclist = {
+    "/catdoor/message" : catdoor_message,
+    "/catdoor/heartbeat" : beatcheck.heartbeat,
+    "/catdoor/solenoids" : catdoor_solenoids,
+    "/catdoor/doorstate" : doorstate.new_state,
+    "/catdoor/proximity" : catdoor_proximity,
+    "/catdoor/battery_v" : catdoor_battery_v
+}
+
 mqttClient = mqtt.Client("CatdoorSubscriber", True, topiclist)
 mqttClient.on_connect = on_connect
 mqttClient.on_message = on_message
 
 mqttClient.connect("172.16.0.11")
-          
+
 mqttClient.loop_forever()
