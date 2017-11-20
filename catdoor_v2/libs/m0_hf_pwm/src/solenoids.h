579e045ae1c9f63d6222dc8e5745831eb1f76037
flupes <lorenzo.flueckiger@gmail.com>
2017-11-20 10:42:40 -0800

    increased the solenoid open max time based on some better experimentation

--- catdoor_v2/libs/m0_hf_pwm/src/solenoids.h
+++ catdoor_v2/libs/m0_hf_pwm/src/solenoids.h
@@ -16,8 +16,19 @@ class Solenoids {
   static const uint8_t PIN_A = 5;
   static const uint8_t PIN_B = 6;
   static const uint8_t PIN_C = 11;
-  static const unsigned long MAX_ON_DURATION_MS = 40000;
-  static const unsigned long COOLDOWN_DURATION_MS = 60000;
+  /*
+    Tests on 2 different solenoids in some sort of enclosure show that
+    under 2.8V, it takes ~5min to reach 50 deg C.
+    One test was inside a reading glass case (padded) and the other inside
+    a ESD safe bag (no air flow).
+    2.8V was choosen as a conservative margin of the 5V PWM signal that is
+    used at half duty cycle.
+    Cooling down after release is a slower process, but the test performed
+    are less significant since the enclosure material and air flow would
+    play larger role.
+  */
+  static const unsigned long MAX_ON_DURATION_MS = 120000;
+  static const unsigned long COOLDOWN_DURATION_MS = 240000;
   static const unsigned long UNJAMMING_DURATION_MS = 4000;
 
   typedef enum {
